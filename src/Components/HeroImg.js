0c0478fbb5bd47fb9b875f887b8441f6d6b5fd61
mohab9248 <mohamad.abbas9248@gmail.com>
2023-07-29 15:31:32 +0300

    background img changing

--- src/Components/HeroImg.js
+++ src/Components/HeroImg.js
@@ -6,7 +6,11 @@ function HeroImg() {
   return (
     <div className="hero">
       <div className="mask">
-        <img className="into-img" src={introimg} alt="intro-img" />
+        <img
+          className="into-img"
+          src="https://images.unsplash.com/photo-1509718443690-d8e2fb3474b7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+          alt="intro-img"
+        />
       </div>
       <div className="content">
         <p>HI, I'M MOHAMAD</p>
